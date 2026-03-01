0a6dbfc1b1b9d1c014e759c7ae1accdd2dd60c16
Mohit Gupta <mg467222@gmail.com>
2026-03-01 17:52:58 +0530

    initial commit

---
+++ portxnect-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from './pages/loginpage'
+
+const App = () => {
+  return (
+    <div className='bg-main-bg h-screen flex items-center justify-center w-screen '>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ portxnect-frontend/src/pages/loginpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const loginpage = () => {
+  return (
+    <div className='w-[90vw]  bg-black/25 grid grid-cols-2 gap-6 border-white/15 border  rounded-2xl h-[80vh] p-6 '>
+        <div className='w-full bg-black/20 border-white/15 border p-6 rounded-2xl'> 
+            
+        </div>
+
+        <div className='w-full bg-black/20 h-full border-white/15 border p-6 rounded-2xl'>
+          
+        </div>
+
+        </div>
+        
+  )
+}
+
+export default loginpage
