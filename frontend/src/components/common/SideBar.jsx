d6af2471afdddbd7e6a1980a37c7baabb63a5630
Mohit Gupta <mg467222@gmail.com>
2026-03-10 22:23:12 +0530

    started creating sidebar

--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -4,9 +4,9 @@ import { AuthContext } from '@/context/AuthContext'
 const SideBar = () => {
   const {logout} = useContext(AuthContext)
   return (
-    <div>
+    <div className=' absolute'>
       hello
-      
+      <br />
         <button onClick={logout}>logout</button> 
     </div> 
   )
--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -3,10 +3,14 @@ import { Outlet } from "react-router-dom"
 
 const DashboardLayout = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 p-6">
-        <Outlet />
+    <div className="flex p-6">
+      <div className="flex p-6 bg-black/80 rounded-2xl w-full h-full">
+      <div className="w-20 border border-white/20 relative rounded-2xl p-4">
+        <Sidebar />
+      </div>
+        <div className="flex-1 p-6">
+          <Outlet />
+        </div>
       </div>
     </div>
   )
