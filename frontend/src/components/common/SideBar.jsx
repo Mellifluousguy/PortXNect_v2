6f6cde846c48f6d78dc4d47b1e5f4b72b89f1457
Mohit Gupta <mg467222@gmail.com>
2026-03-11 22:01:42 +0530

    custom designing sidebar

--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -1,16 +1,42 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '@/context/AuthContext'
+import { Home } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
+
+
+
 
 const SideBar = () => {
-  const {logout} = useContext(AuthContext)
+  const { logout } = useContext(AuthContext)
+
+  const navItem = (to, Icon, label) => (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `flex items-center gap-4 px-4 py-3 rounded-full transition-all duration-300
+        ${isActive
+          ? "bg-linear-to-r from-emerald-800/70 to-emerald-600/40"
+          : "bg-white/5 hover:bg-white/10"
+        }`
+      }
+    >
+      <Icon size={22} />
+      <span className="whitespace-nowrap  group-hover:opacity-100 transition-opacity duration-300">
+        {label}
+      </span>
+    </NavLink>
+  )
+
   return (
-    <div className=' absolute  flex items-center flex-col w-full'>
-      <img src="/assets/logo.png" alt="Logo" /> 
-      
+    <div className='  flex items-center flex-col w-full'>
+      <img src="/assets/logo.png" alt="Logo" />
+
+      {navItem("/dashboard", Home, "Home")}
 
 
-        <button onClick={logout}>logout</button> 
-    </div> 
+
+      <button onClick={logout}>logout</button>
+    </div>
   )
 }
 
--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,13 +1,13 @@
-import Sidebar from "@/components/common/Sidebar"
+import Sidebar from "@/components/common/SideBar.jsx"
 import { Outlet } from "react-router-dom"
 
 const DashboardLayout = () => {
   return (
     <div className="flex p-6">
       <div className="flex p-6 bg-black/80 rounded-2xl w-full h-full">
-      <div className="w-20 border border-white/20 relative rounded-2xl pt-6">
+      <div className="w-20 border border-white/20 rounded-2xl pt-6">
         <Sidebar />
-      </div>ff
+      </div>››
         <div className="flex-1 p-6">
           <Outlet />
         </div>
