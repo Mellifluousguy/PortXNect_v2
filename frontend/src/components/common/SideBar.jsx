d47d7a2be99247164542d2da52db14ecb84a4cfb
Mohit Gupta <mg467222@gmail.com>
2026-03-12 22:21:28 +0530

    full complete sidebar

--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -13,7 +13,7 @@ const SideBar = () => {
     <NavLink
       to={to}
       className={({ isActive }) =>
-        `flex items-center group-hover:gap-4 group-hover:px-6 h-12 w-12  group-hover:justify-start group-hover:w-3xs justify-center rounded-full transition-all duration-10
+        `flex items-center group-hover:gap-4 group-hover:px-6 h-12 w-12  group-hover:justify-start group-hover:w-full justify-center rounded-full transition-all duration-10
         ${isActive
           ? "bg-linear-to-r from-emerald-800/70 to-emerald-600/40"
           : "bg-white/5 hover:bg-white/10"
@@ -21,23 +21,41 @@ const SideBar = () => {
       }
     >
       <Icon size={22} strokeWidth={1.5} />
-      <span className="whitespace-nowrap w-0 group-hover:w-max overflow-hidden transition-all duration-300">
+      <span className="whitespace-nowrap w-0 scale-0 group-hover:scale-100 group-hover:w-max overflow-hidden transition-all duration-500">
         {label}
       </span>
     </NavLink>
   )
 
   return (
-    <div className='group hover:px-4 h-full py-10 flex items-center flex-col w-20 hover:w-max transition-all duration-300 overflow-hidden'>
+    <div className='group px-4 h-full py-10 flex items-center flex-col w-20 hover:w-64 transition-all duration-500 overflow-hidden'>
       <div className='mb-10'>
 
-        <img src="/assets/logo.png" alt="Logo" className='w-10' />
-        <img src="/logo-tagline.svg" alt="" />
+        <div className="mb-10 h-10 flex w-full items-center group-hover:px-0 justify-center">
 
+          {/* Small Logo */}
+          <img
+            src="/assets/logo.png"
+            alt="Logo"
+            className="w-10 transition-all duration-500 ease-in-out
+               opacity-100 scale-100
+               group-hover:opacity-0 group-hover:scale-0 group-hover:w-0"
+          />
+
+          {/* Full Logo */}
+          <img
+            src="/logo-tagline.svg"
+            alt="Full Logo"
+            className="w-0 group-hover:w-full transition-all duration-500 ease-in-out
+               opacity-0 scale-95
+               group-hover:opacity-100 group-hover:scale-100"
+          />
+
+        </div>
       </div>
-      <div className='flex flex-col justify-between h-full'>
+      <div className='flex flex-col justify-between items-center w-full h-full'>
 
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col w-full gap-4'>
           {navItem("/dashboard", Home, "Home")}
           {navItem("/connect", Users, "Connect")}
           {navItem("/messages", MessageCircle, "Messages")}
@@ -47,14 +65,13 @@ const SideBar = () => {
 
 
 
-        <button onClick={logout} className={`flex bg-white/5 hover:bg-white/10 items-center group-hover:gap-4 group-hover:px-6 h-12 w-12  group-hover:justify-start group-hover:w-3xs justify-center rounded-full transition-all duration-10
-`}>
-          <LogOut size={22} strokeWidth={1.5} />
-          <span className="whitespace-nowrap w-0 group-hover:w-max overflow-hidden transition-all duration-300">
-            Logout
-          </span>
-        </button>
-      </div>
+          <button onClick={logout} className={`flex  bg-white/5 hover:bg-white/10 items-center group-hover:gap-4 group-hover:px-6 h-12 w-12  group-hover:justify-start group-hover:w-full justify-center rounded-full transition-all duration-10`}>
+            <LogOut size={22} strokeWidth={1.5} />
+            <span className="whitespace-nowrap w-0 group-hover:w-max overflow-hidden transition-all duration-500">
+              Logout
+            </span>
+          </button>
+        </div>
     </div>
   )
 }
